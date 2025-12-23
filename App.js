9d83ee27c450dce91db12310f4bac85a874b58a6
Adarsh162-debug <adarshogi14@gmail.com>
2025-12-23 09:49:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import Chai from "./chai.js";
+function App() {
+  return (
+    /*<h1>
+      chai aur code | my name is adarsh . i known all about how to create file in react and vite@latest.
+       how to run file in react file.
+       all about about the package.json folder
+       and project also run on web
+
+    </h1>*/
+    <><Chai/></>
+    
+  )
+}
+
+export default App;
---
+++ chai.js
@@ -0,0 +1,6 @@
+ function Chai(){
+    return(
+        <h1>hey this is chai code</h1>
+    )
+ }
+ export default Chai
